c1993194b8d819c01b323f04103362a4bc2df79d
mrtswd <60158608+mrtswd@users.noreply.github.com>
2022-11-13 19:48:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.header ('Line chart')
+
+chart data = pd.DataFrame(
+  np.random.randn(20,3),
+  columns=['viens','divi','tris'])
+
+st.line_chart(chart_data)
